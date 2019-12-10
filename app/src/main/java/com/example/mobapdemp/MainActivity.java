19c1368bed071f94c286cb4b3490bfde49385e39
RaeanneVillanueva <raeanne1999@gmail.com>
2019-12-10 16:32:37 +0800

    added dependency

--- app/src/main/java/com/example/mobapdemp/MainActivity.java
+++ app/src/main/java/com/example/mobapdemp/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.mobapdemp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
+import com.yuyakaido.android.cardstackview.CardStackListener;
+import com.yuyakaido.android.cardstackview.CardStackView;
+import com.yuyakaido.android.cardstackview.Direction;
+
+public class MainActivity extends AppCompatActivity implements CardStackListener {
+
+    private CardStackLayoutManager cardStackLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardStackLayoutManager = new CardStackLayoutManager(this);
+
+
+
+        CardStackView cardStackView = findViewById(R.id.card_stack_view);
+
+
+
+    }
+
+    @Override
+    public void onCardDragging(Direction direction, float ratio) {
+
+    }
+
+    @Override
+    public void onCardSwiped(Direction direction) {
+
+    }
+
+    @Override
+    public void onCardRewound() {
+
+    }
+
+    @Override
+    public void onCardCanceled() {
+
+    }
+
+    @Override
+    public void onCardAppeared(View view, int position) {
+
+    }
+
+    @Override
+    public void onCardDisappeared(View view, int position) {
+
     }
 }
